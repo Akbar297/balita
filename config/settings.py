9251a77ab4e60e23f92c65620a29608f3dfb1284
Akbar <148070512+Akbar297@users.noreply.github.com>
2024-07-04 19:29:53 +0500

    fixed baggs

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a-+n5%b9wc73fe59$%*vqzq-w_5@72l$=bste1p6+w+f152zhq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
